d69eec30309b9367ab2e5f6e705b0ab9f1bd2daf
unknown <joshuamackie10@googlemail.com>
2023-04-19 11:54:38 +0100

    formating the projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styles from "../styles/projects.module.css"
 import Nav from "../components/Nav.js"
 import Rise from "../components/Rise.js"
 import Self from "../components/Self.js"
@@ -9,13 +10,24 @@ export default function Projects() {
     return (
         <>
             <Nav />
-            <h2>Joshua Mackie</h2>
-            <a href="https://github.com/MackieJG">GitHub</a>
-            <h1>Projects</h1>
-            <Rise />
-            <Self />
-            <CIM />
-
+            <div className={styles.pageWrapper}>
+                <div className={styles.profileContainer}>
+                    <h2>Joshua Mackie</h2>
+                    <a href="https://github.com/MackieJG">GitHub</a>
+                </div>
+                <div className={styles.title}>
+                    <h1>Projects</h1>
+                </div>
+                <div className={styles.riseContainer}>
+                    <Rise />
+                </div>
+                <div className={styles.selfContainer}>
+                    <Self />
+                </div>
+                <div className={styles.cimContainer}>
+                    <CIM />
+                </div>
+            </div>
         </>
     )
 }
