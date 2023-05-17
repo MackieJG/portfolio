d58ebdad80d1be4f142d55f1dc6d9bf378183cac
unknown <joshuamackie10@googlemail.com>
2023-05-17 15:56:47 +0100

    updating the favicon to my logo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,13 @@
 import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
+import Head from "next/head"
 
 export default function App({ Component, pageProps }) {
   return (
   <main>
+    <Head>
+      <link rel="shortcut icon" href="/MJG.png"/>
+    </Head>
     <Component {...pageProps} />
     <Analytics />
   </main>
