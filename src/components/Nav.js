b6d92cfb4bad6a45ab9def2f25044f8ac37b4294
unknown <joshuamackie10@googlemail.com>
2023-11-29 15:59:40 +0000

    created a little design for the nav bar stripped it back

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,30 @@
-import Link from "next/link"
-import styles from "../styles/Nav.module.css"
-export default function Nav () {
+import Link from "next/link";
 
-    return (
-        <>
-            <div className={styles.container}>
-                <Link href="/">Home</Link>
-                <Link href="/projects">Projects</Link>
-                <Link href="/profile">About Me</Link>
-            </div>
-        </>
-    )
-};
+export default function Nav() {
+  return (
+    <>
+      <div className="flex flex-row justify-center space-x-4 p-4 text-base">
+        <Link href="/" className="justify-self-auto hover:underline-offset-4">
+          Home
+        </Link>
+        <Link href="/projects" className="justify-self-auto">
+          Projects
+        </Link>
+        <Link href="/profile" className="justify-self-auto">
+          About Me
+        </Link>
+      </div>
+    </>
+  );
+}
+
+// .container {
+//     display: flex;
+//     justify-content: space-evenly;
+//     background-color: #113c55;
+//     align-items: center;
+//     font-family: "Trebuchet MS", Verdana, sans-serif;
+//     color: #fdf9f7;
+//     font-size: 25px;
+//     height: 80px;
+// }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,69 +1,69 @@
-import React, {useState} from "react"
-import ReactPlayer from "react-player"
-import styles from "../styles/welcome.module.css"
-import Nav from "../components/Nav.js"
-import ScrollBar from "../components/ScrollBar.js"
-import {SocialIcon} from "react-social-icons"
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import styles from "../styles/welcome.module.css";
+import Nav from "../components/Nav.js";
+import { SocialIcon } from "react-social-icons";
 
 export default function Home() {
+  const [videoId, setVideoId] = useState(null);
 
-    const [videoId, setVideoId] = useState(null);
+  const handleCoffeeProjectClick = () => {
+    setVideoId("nWTkNJm1lsY");
+  };
 
-    const handleCoffeeProjectClick = () => {
-        setVideoId("nWTkNJm1lsY")
-    };
+  const handleSelfProjectClick = () => {
+    setVideoId("e27rc_Zbops");
+  };
+  const handleRiseProjectClick = () => {
+    setVideoId("QDKnOq5sEZI");
+  };
 
-    const handleSelfProjectClick = () => {
-        setVideoId("e27rc_Zbops")
-    };
-    const handleRiseProjectClick = () => {
-        setVideoId("QDKnOq5sEZI")
-    };
+  return (
+    <>
+      <Nav />
+      <div className={styles.pageWrapper}>
+        <div className={styles.profile}>
+          <p>
+            Hi, I'm Joshua Mackie, Software Developer. Welcome to my Portfolio!
+          </p>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.aboutMe}>
+            {videoId ? (
+              <ReactPlayer
+                width="100%"
+                height="100%"
+                url={`https://www.youtube.com/watch?v=${videoId}`}
+                playing={Boolean("true")}
+              />
+            ) : (
+              <p className={styles.projectPreview}>Project Previews</p>
+            )}
+          </div>
 
-  
-    return (
-        <>
-        < Nav />
-        <ScrollBar />
-            <div className={styles.pageWrapper}>
-                <div className={styles.profile}>
-                    <p>Hi, I'm Joshua Mackie, Software Developer. Welcome to my Portfolio!</p>
-                 </div>
-                <div className={styles.container}>
-                    <div className={styles.aboutMe}>
-                        { videoId ? (
-                            <ReactPlayer
-                                width="100%"
-                                height="100%"
-                                url={`https://www.youtube.com/watch?v=${videoId}`}
-                                playing={Boolean("true")}
-                            />
-                        ) : (
-                                <p className={styles.projectPreview}>
-                                    Project Previews
-                                </p>
-                        )}
-                
-                    </div>
-                
-                    <div className={styles.linkMe}>
-
-                        <button className={styles.button} onClick={handleCoffeeProjectClick}>CIM</button>
-                        <button className={styles.button} onClick={handleSelfProjectClick}>.Self</button>
-                        <button className={styles.button} onClick={handleRiseProjectClick}>Rise</button>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.socialIcons}>
-                <div className={styles.linkedIn}>
-                    <SocialIcon url="https://www.linkedin.com/in/mackiejg/"/>
-                </div>
-                <div className={styles.gitHub}>
-                    <SocialIcon url="https://github.com/MackieJG"
-                        bgColor="black"
-                    />
-                 </div>
-            </div>
-        </>
-    )
+          <div className={styles.linkMe}>
+            <button
+              className={styles.button}
+              onClick={handleCoffeeProjectClick}>
+              CIM
+            </button>
+            <button className={styles.button} onClick={handleSelfProjectClick}>
+              .Self
+            </button>
+            <button className={styles.button} onClick={handleRiseProjectClick}>
+              Rise
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.socialIcons}>
+        <div className={styles.linkedIn}>
+          <SocialIcon url="https://www.linkedin.com/in/mackiejg/" />
+        </div>
+        <div className={styles.gitHub}>
+          <SocialIcon url="https://github.com/MackieJG" bgColor="black" />
+        </div>
+      </div>
+    </>
+  );
 }
